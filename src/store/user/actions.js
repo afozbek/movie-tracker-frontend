5739cd70175d5f0d1eaac95ec55627734bd16b1f
afozbek <furkanozbek1995@gmail.com>
2020-09-01 19:17:45 +0300

    action lar eklendi

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,6 +1,40 @@
-import { LOGIN_STARTED, LOGIN_SUCCESS, LOGIN_FAILED } from "./types";
+import {
+  LOGIN_STARTED,
+  LOGIN_SUCCESS,
+  LOGIN_FAILED,
+  LOGOUT_SUCCESS,
+} from "./types";
 
 import axios from "../../axios-instance";
+
+export const checkUserAuthentication = () => {
+  return (dispatch) => {
+    const jwtToken = localStorage.getItem("jwttoken") || null;
+
+    if (jwtToken) {
+      axios
+        .get("/checkUserAuthentication", {
+          params: { jwtToken },
+        })
+        .then((res) => {
+          const userData = {
+            fullName: res.data.user.firstName + " " + res.data.user.lastName,
+            user: {
+              token: jwtToken,
+              username: res.data.username,
+              authenticated: true,
+            },
+          };
+          dispatch(loginSuccess(userData));
+        })
+        .catch((err) => {
+          console.log(err);
+
+          dispatch(loginFailed(err.message));
+        });
+    }
+  };
+};
 
 export const login = (username, password, history) => {
   return (dispatch) => {
@@ -11,10 +45,10 @@ export const login = (username, password, history) => {
       .then((res) => {
         dispatch(loginSuccess(res.data));
 
-        // history.push("/");
+        history.push("/");
       })
       .catch((err) => {
-        dispatch(loginFailure(err.message));
+        dispatch(loginFailed(err.message));
       });
 
     // history.push("/");
@@ -32,9 +66,14 @@ const loginStarted = () => ({
   type: LOGIN_STARTED,
 });
 
-const loginFailure = (error) => ({
+const loginFailed = (error) => ({
   type: LOGIN_FAILED,
   payload: {
     error,
   },
+});
+
+export const logoutHandler = (history) => ({
+  type: LOGOUT_SUCCESS,
+  payload: history,
 });
