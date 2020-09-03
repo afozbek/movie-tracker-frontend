bd80931715bd23e54e5a9034495e2db33db5ed90
afozbek <furkanozbek1995@gmail.com>
2020-09-03 22:45:14 +0300

    düzeltildi

--- src/Components/Home/MovieCard/MovieCard.js
+++ src/Components/Home/MovieCard/MovieCard.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import { NavLink } from "react-router-dom";
 
-import { FontAwesomeIcon } from "act-fontawesome";
-import { faStar } from "ee-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { genres } from "../../../utils";
 
 import "./MovieCard.scss";
