7ec9a8adf2f70cbbbe0a068beef1a864b2456ad7
afozbek <furkanozbek1995@gmail.com>
2020-09-03 22:15:32 +0300

    kullanmıyan useState kaldırıldı

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 import { connect } from "react-redux";
 import { getMovies } from "../../store/movies/actions";
